3d7005ccb110850ab0a1b784349199ce98ed3097
adriancesurform <adrian.s465656@cesurformacion.com>
2025-01-23 08:07:37 +0100

    instalar tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Tailwind/**/*.{html,js}"], // Escanea los archivos HTML y JS dentro de la carpeta Tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
