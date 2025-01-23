07559cccb51f8cd9ff82650565bcb6e3160d937c
adriancesurform <adrian.s465656@cesurformacion.com>
2025-01-23 11:16:52 +0100

    nuevo commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Tailwind/**/*.{html,js}"], // Escanea los archivos HTML y JS dentro de la carpeta Tailwind
+  content: ["./Tailwind/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
